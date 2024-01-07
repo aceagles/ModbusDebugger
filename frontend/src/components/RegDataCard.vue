c75d1a38644d1dd07105a2c390c03c24ba0b9821
Adam Eagles <ad.c.eagles@gmail.com>
2024-01-07 12:45:28 +0000

    Attempting context menu

--- frontend/src/components/RegDataCard.vue
+++ frontend/src/components/RegDataCard.vue
@@ -1,19 +1,24 @@
 <script setup>
+import VueContext from 'vue-context';
 const props = defineProps(['data']);
+function handleClick(event, item) {
+  console.log(item, event)
+}
 </script>
 
 <template>
-    <div class="border m-2 p-2 bg-light container">
-    <div class="d-flex">
-      <div class="d-inline px-2 ">Register: <br> Value: </div>
-      <div class="d-flex flex-row flex-wrap">
-      
-        <div class="border px-2 text-center bold" v-for="(column, columnIndex) in data.RegData" :key="columnIndex">
-          <b>{{ columnIndex + data.startReg }}</b>
-          <br>
-          {{ column }}
-        </div>
-      </div>
+  <div class="border m-2 p-2 bg-light container">
+  <div class="d-flex">
+    <div class="d-inline px-2 ">Register: <br> Value: </div>
+    <div class="d-flex flex-row flex-wrap">
+    
+      <div class="border px-2 text-center bold" v-for="(column, columnIndex) in data.RegData" :key="columnIndex" @contextmenu.prevent.stop="handleClick($event, item)">
+        <b>{{ columnIndex + data.startReg }}</b>
+        <br>
+        {{ column }}
+      </div>  
     </div>
-    </div>
+  </div>
+
+  </div>
 </template>
